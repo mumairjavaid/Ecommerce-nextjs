a1aa6e6c6808c5fe4722a5f300006f1e7ede3e80
Umair <umairjavaid01@gmail.com>
2023-07-22 23:54:28 +0500

    Additional changes added to pages

---
+++ models.js/Checkout.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    productId: { type: Number, required: true },
+    price: { type: Number, required: true },
+    color: { type: String },
+    variant: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
